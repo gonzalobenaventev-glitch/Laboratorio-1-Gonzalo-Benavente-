ca5f712508f4887eca6cf52b2cfed9bc8900fd89
gonzalobenaventev-glitch <gonzalo.benavente.v@mail.pucv.cl>
2026-03-23 17:47:42 -0300

    Add files via upload

---
+++ exercises(1).c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "arraylist.h"
+#include "stack.h"
+
+//#include "exercises.h"
+
+//Funciones auxiliares que puedes utilizar para debuggear tus ejercicios
+//NO MODIFICAR
+void imprime_lista(List *L) {
+   int *dato;
+   dato = (int*)first(L);
+   printf("[");
+   while(dato != NULL) {
+      printf("%d ", *dato);
+      dato = (int*)next(L);
+   }
+   printf("]\n");
+
+}
+
+//Ojo que la pila se vacía al imprimir y se imprime en orden inverso
+//NO MODIFICAR
+void imprime_y_vacia_pila(Stack *P) {
+   void *dato;
+   printf("[");
+   while((dato = pop(P)) != NULL) {
+      printf("%d ", *(int*)dato);
+   }
+   printf("]\n");
+}
+
+/* 
+Ejercicio 1.
+Crea una Lista y agrega punteros a elementos del 1 al 10.
+Recuerda que la lista almacena punteros, por lo que
+debes reservar memoria para cada elemento que agregues.
+Al finalizar retorna la lista creada.
+*/
+
+List* crea_lista() 
+{
+   List* L = create_list();
+
+   first(L);
+
+   for (int i = 1; i < 10; i++)
+      {
+         next(L);
+         i++;
+      }
+
+   return L;
+   
+}
+
+/*
+Ejercicio 2.
+Crea una función que reciba una lista de enteros (int*) y 
+retorne la suma de sus elementos.
+*/
+int sumaLista(List *L) 
+{
+   
+   //int nueva_lista = 0;
+
+   int suma = 0;
+
+   /*first(L);
+
+   pushFront(nueva_lista, &L);
+
+   while (next(L) != NULL)
+      {
+         for (int i = 0; i < get_size(L); i++)
+         {
+            suma += nueva_lista[i];
+         
+            next(L);
+
+            pushCurrent(nueva_lista, &L);
+
+            i++;
+
+         }
+      }
+      */
+   
+   return suma;
+}
+
+/*
+Ejercicio 3.
+Crea una función que reciba una lista de punteros a int (int*) y
+un entero elem. La función debe eliminar todos los elementos 
+de la lista que sean iguales a elem.
+Asume que popCurrent luego de eliminar un elemento se
+posiciona en el elemento anterior.
+*/
+
+void eliminaElementos(List*L, int elem)
+{
+   /*
+   
+   first(L);
+   
+   while (next(L) != NULL)
+      {
+         if (L == elem)
+         {
+            popCurrent(L);
+         }
+
+         else
+         {
+            next(L);
+         }
+      }
+   */
+}
+
+/*
+Ejercicio 4.
+La función copia los punteros de la pila P1 en la pila P2.
+El orden de ambas pilas se debe mantener.
+Puedes usar una pila auxiliar.
+*/
+
+void copia_pila(Stack* P1, Stack* P2) 
+{
+   /*Stack* aux = create_stack();
+
+   while (aux == NULL)
+      {
+         push(P1, aux);
+      }
+
+   while (P2 == NULL)
+      {
+         push(aux, P2);
+         pop(aux);
+      }
+
+   free(aux);
+
+   return P1, P2;*/
+   
+}
+
+/*
+Ejercicio 5.
+La función verifica si la cadena de entrada tiene sus 
+paraéntesis balanceados. Retorna 1 si están balanceados,
+0 en caso contrario.
+*/
+
+int parentesisBalanceados(char *cadena) 
+{
+   /*Stack* pila = create_stack();
+
+   int contador_der = 0;
+
+   int contador_izq = 0;
+
+   for (int i = 0; i < sizeof(cadena); i++)
+      {
+         if (cadena[i] == "(" || cadena[i] == "{" || cadena[i] == "[")
+         {
+            contador_izq++;
+            for (int j = sizeof(cadena) - i; j < sizeof(cadena); j++)
+               {
+                  if (cadena[i] == "(" && cadena[j] == ")")
+                  {
+                     push(pila, (cadena[i], cadena[j]));
+                     contador_der++;
+                  }
+                  else if (cadena[i] == "{" && cadena[j] == "}")
+                  {
+                     push(pila, (cadena[i], cadena[j]));
+                     contador_der++;
+                  }
+                  else if (cadena[i] == "[" && cadena[j] == "]")
+                  {
+                     push(pila, (cadena[i], cadena[j]));
+                     contador_der++;
+                  }
+                  else
+                  {
+                     j++;
+                  }
+               }
+         }
+         i++;
+      }
+
+   if(contador_izq == contador_der)
+   {
+      return 1;
+   }
+   else
+   {
+      return 0;
+   }
+
+   free(pila);*/
+}
